2999254601f9bf0dd52d5bfc078f0d27808aa9d5
RyanWonderPet <131748577+RyanWonderPet@users.noreply.github.com>
2023-05-02 15:32:25 +0800

    initial commit

---
+++ footer.php
@@ -0,0 +1,157 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:600,900" rel="stylesheet">
+    <link rel="stylesheet" href="<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    
+
+  </head>
+  <body>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/ccb46faed6.js" crossorigin="anonymous"></script>
+<!-- Footer -->
+<footer class="text-center text-lg-start text-muted" style="padding: 1px; background-color: rgb(32, 32, 32);
+    color: rgb(255, 255, 25");">
+  <!-- Section: Social media -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <main class="text-white">
+            <i class="me-3 text-secondary"></i>COMPANY
+            <main>
+          </h6>
+          <p class="text-white-50">
+           A108 Adam Street <br>
+           New York, NY 5350225<br>
+           United States
+          <br>
+</br>
+           Phone: +1 5589 55488 55
+           Email: info@example.com
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 text-white">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Useful Links
+          </h6>
+          <main class="text-white-50">
+          <p>
+            <a href="#!" class="text-reset">Home</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">About Us</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Services</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Terms of service</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Privacy policy</a>
+          </p>
+        </main>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4 text-white">
+            Our Services
+          </h6>
+          <main class="text-white-50 text-muted">
+          <p>
+            <a href="#!" class="text-reset">Web Design</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Web Development</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Product Management</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Marketing</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Graphic Design</a>
+          </p>
+          </main>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 text-left mb-md-0 mb-4 text-white">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">Join Our Newsletters</h6>
+          <p class="text-white-50">Tamen quem nulla quae legam multos aute sint culpa<br>legam noster magna </p>
+    
+          </p>
+          <div class="input-group">
+            <input type="search" class="form-control-sm rounded" style="margin-top: 4px;" aria-label="Search" aria-describedby="search-addon" />
+            <button type="button" class="btn bg-success">Subscribe</button>
+          </div>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-left p-4 text-white" style="background-color: rgba(0, 0, 0, 0.952);">
+    © 2021 Copyright:
+    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    <div class="col-md-3 col-lg-2 col-xl-2 float-right mb-4">
+    <div>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
+</body>
---
+++ header.php
@@ -0,0 +1,55 @@
+<header>
+ <nav class="navbar navbar-expand-lg navbar-light">
+  <a class="navbar-brand font-weight-bold" style="margin-left: 150px" href="#"><span style="color: green">TEST</span>SITE</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto" style="margin-right: 200px">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">HOME <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item dropdown active">
+        <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+          ABOUT
+        </a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="services.php">SERVICES <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="portfolio.php">PORTFOLIO <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">PRICING <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">BLOG <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">CONTACT <span class="sr-only">(current)</span></i></a>
+      </li> 
+      <li class="nav-item" style="margin-top: 6px ">|</li> 
+      <li class="nav-item">
+      <a class="nav-link" href="#"><i class="fa-brands fa-twitter"></i></i> <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+      <a class="nav-link" href="#"><i class="fa-brands fa-facebook"></i> <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+      <a class="nav-link" href="#"><i class="fa-brands fa-instagram"></i> <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+      <a class="nav-link" href="#"><i class="fa-brands fa-linkedin"></i> <span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</header>
