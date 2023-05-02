2d52b514cefc3fedf90b1bdbe6fc57f1cb19df4b
RyanWonderPet <131748577+RyanWonderPet@users.noreply.github.com>
2023-05-02 22:47:38 +0800

    new query

---
+++ index.php
@@ -0,0 +1,396 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Test Site</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="main.css">
+    </head>
+
+    <header>
+ <nav class="navbar navbar-expand-lg navbar-light">
+  <a class="navbar-brand font-weight-bold" style="margin-left: 150px" href="#"><span style="color: green">TEST</span>SITE</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto" style="margin-right: 200px">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">HOME<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+          ABOUT
+        </a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="services.php">SERVICES <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="portfolio.php">PORTFOLIO <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">PRICING <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">BLOG <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">CONTACT <span class="sr-only">(current)</span></i></a>
+      </li> 
+      <li class="nav-item" style="margin-top: 6px ">|</li> 
+      <li class="nav-item">
+      <a class="nav-link" href="#"><i class="fa-brands fa-twitter"></i></i> <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+      <a class="nav-link" href="#"><i class="fa-brands fa-facebook"></i> <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+      <a class="nav-link" href="#"><i class="fa-brands fa-instagram"></i> <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+      <a class="nav-link" href="#"><i class="fa-brands fa-linkedin"></i> <span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+ </header>
+    <body>
+
+<div class="mid">
+<img class="embed-responsive" src="img/business.jpg">
+<div id="containerr" class="container">
+      <div class=" text">
+        <h2 style="font-size: 19px;">Lorem Ipsum Dolor</h2>
+        <p style="font-size: 12px;">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architectol.</p>
+        <div class="d-flex justify-content-center div_button">
+          <button type="button" class="btn btn-outline-success text-white">Read More</button>
+      </div>
+      </div>
+    </div>
+</div>
+
+
+
+ <div class="row">
+    <div id="column1" class="column" style="background-color:white;" style="width: 50%;">
+      <h2 style="font-size: 27px;">EUM IPSAM LABORUM DELENITI</h2>
+      <p style="font-size: 14px;">voluptatem dinissimos provident quasi corporis <br>voluptates sit assum perenda sruen jonee trave</p>
+    </div>
+    <div id="column2" class="column" style="background-color:white;" style="width: 50%;">
+     <p style="font-size: 13px;">Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in <br>reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Exepteur<br>sint occaecat cupidatat non proident, sunt in culpa qui official deserunt mollit anim id <br>est laborum</p>
+     
+     <p>✅ Ullamco laboris nisi ut aliquip ex ea commodo consequa</p>
+     <p>✅ Duis aute irure dolor in reprehenderit in voluptate velit</p>
+     <p>✅ Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in</p>
+
+     <p style=" font-style: italic;">Lorem Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt <br>ut labore et dolore magno aliqua.</p>
+    </div>
+  </div>
+ 
+  <div class="box">
+    <div id="newcontain" class="container">
+     	<div class="row">
+			 
+			    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+               
+					<div id="boxes" class="box-part text-center">
+                        
+            <img class="pic1" src="img/1st.png" class="card-img-top" alt="..."> 
+                        
+						<div class="title">
+							<h4>Lorem Ipsum</h4>
+						</div>
+                        
+						<div class="text">
+							<span>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</span>
+						</div>
+                        
+					
+                        
+					 </div>
+				</div>	 
+				
+				 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+               
+					<div id="midbox" class="box-part text-center">
+					    
+					    <i class="fa fa-twitter fa-3x" aria-hidden="true"></i>
+                    
+              <img class="pic1" src="img/pic 2.png" class="card-img-top" alt="..."> 
+                
+						<div class="title">
+							<h4>Twitter</h4>
+						</div>
+                        
+						<div class="text">
+							<span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+						</div>
+                        
+						
+                        
+					 </div>
+				</div>	 
+				
+				 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+               
+					<div id="boxess" class="box-part text-center">
+                        
+                        <i class="fa fa-facebook fa-3x" aria-hidden="true"></i>
+                        
+                        <img class="pic1" src="img/pic3.png" class="card-img-top" alt="..."> 
+						<div class="title">
+							<h4>Facebook</h4>
+						</div>
+                        
+						<div class="text">
+							<span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+						</div>
+                        
+					
+                      
+					 </div>
+				</div>	 
+				
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+               
+					<div id="bot1" class="box-part text-center">
+                        
+                        <i class="fa fa-pinterest-p fa-3x" aria-hidden="true"></i>
+                        <img class="pic1" src="img/pic4.png" class="card-img-top" alt="..."> 
+						<div class="title">
+							<h4>Pinterest</h4>
+						</div>
+                        
+						<div class="text">
+							<span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+						</div>
+                        
+                        
+					 </div>
+				</div>	 
+				
+				 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+               
+					<div id="bot2" class="box-part text-center">
+					    
+					    <i class="fa fa-google-plus fa-3x" aria-hidden="true"></i>
+
+              <img class="pic1" src="img/pic5.png" class="card-img-top" alt="..."> 
+						<div class="title">
+							<h4>Google</h4>
+						</div>
+                        
+						<div class="text">
+							<span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+						</div>
+                        
+						
+                        
+					 </div>
+				</div>	 
+				
+				 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+               
+					<div id="bot3" class="box-part text-center">
+                        
+                        <i class="fa fa-github fa-3x" aria-hidden="true"></i>
+                        <img class="pic1" src="img/pic6.png" class="card-img-top" alt="..."> 
+						<div class="title">
+							<h4>Github</h4>
+						</div>
+                        
+						<div class="text">
+							<span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+						</div>
+                        
+						
+                        
+					 </div>
+				</div>
+		
+		</div>		
+    </div>
+</div>
+  
+<div class="d-flex justify-content-center">
+<button type="button" class="btn">ALL</button>
+<button type="button" class="btn">APP</button>
+<button type="button" class="btn">CARD</button>
+<button type="button" class="btn">WEB</button>
+</div>  
+
+<div class="container">
+  <img id="image" src="img/images.png" class="img-fluid mx-auto d-block" alt="Responsive image">
+</div>
+</div>
+
+<div>
+<h2 class="d-flex justify-content-center font-weight-bold" style="margin-top: 5%;">CLIENTS</h2>
+</div>
+
+
+</div>
+
+<div class="container" style="margin-top: 40px;">
+  <img id="image" src="img/fourbox.png" class="img-fluid mx-auto d-block" alt="Responsive image" style="margin-bottom: 40px;">
+</div>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+    </body>
+    <script src="https://kit.fontawesome.com/ccb46faed6.js" crossorigin="anonymous"></script>
+
+
+
+</body>
+
+
+<!-- Footer -->
+<footer class="text-center text-lg-start text-muted" style="padding: 1px; background-color: rgb(32, 32, 32);
+    color: rgb(255, 255, 25");">
+  <!-- Section: Social media -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <main class="text-white">
+            <i class="me-3 text-secondary"></i>COMPANY
+            <main>
+          </h6>
+          <p class="text-white-50">
+           A108 Adam Street <br>
+           New York, NY 5350225<br>
+           United States
+          <br>
+</br>
+           Phone: +1 5589 55488 55
+           Email: info@example.com
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 text-white">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Useful Links
+          </h6>
+          <main class="text-white-50">
+          <p>
+            <a href="#!" class="text-reset">Home</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">About Us</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Services</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Terms of service</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Privacy policy</a>
+          </p>
+        </main>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4 text-white">
+            Our Services
+          </h6>
+          <main class="text-white-50 text-muted">
+          <p>
+            <a href="#!" class="text-reset">Web Design</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Web Development</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Product Management</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Marketing</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Graphic Design</a>
+          </p>
+          </main>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 text-left mb-md-0 mb-4 text-white">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">Join Our Newsletters</h6>
+          <p class="text-white-50">Tamen quem nulla quae legam multos aute sint culpa<br>legam noster magna </p>
+    
+          </p>
+          <div class="input-group">
+            <input type="search" class="form-control-sm rounded" style="margin-top: 4px;" aria-label="Search" aria-describedby="search-addon" />
+            <button type="button" class="btn bg-success">Subscribe</button>
+          </div>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-left p-4 text-white" style="background-color: rgba(0, 0, 0, 0.952);">
+    © 2021 Copyright:
+    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    <div class="col-md-3 col-lg-2 col-xl-2 float-right mb-4">
+    <div>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
+    </html>
