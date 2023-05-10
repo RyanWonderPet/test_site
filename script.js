900855dd393c61172267005feeba7e283dc04f3e
RyanWonderPet <131748577+RyanWonderPet@users.noreply.github.com>
2023-05-10 11:32:25 +0800

    Deleted the file from the git repository

--- script.js
+++
@@ -1,8 +0,0 @@
-const navLinkEls = document.querySelectorAll('.nav-link');
-const windowPathname = window.location.pathname;
-
-navLinkEls.forEach(navLinkEL => {
-    if (navLinkEl.href.includes(windowPathname)) {
-        navLinkEl.classlist.add('active');
-    }
-});
